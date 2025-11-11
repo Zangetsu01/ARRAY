89d72bed55e28e4c35a39c1264a7882fcaf89422
vaishnavisuresh588-spec <vaishnavisuresh588@gmail.com>
2025-11-11 11:52:56 +0530

    one

--- array.py
+++ array.py
@@ -1,12 +1,9 @@
-scores = input("Enter scores separated by spaces: ").split()
-scores = [float(s) for s in scores]
+import sys
+
+scores = [float(x) for x in sys.argv[1:]]
 
 total = sum(scores)
 average = total / len(scores)
-maximum = max(scores)
-minimum = min(scores)
 
 print("Sum =", total)
 print("Average =", average)
-print("Maximum =", maximum)
-print("Minimum =", minimum)
