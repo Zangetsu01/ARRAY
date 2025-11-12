aeafa3a45be9256bca613f2eaf2242db0f630cf9
vaishnavisuresh588-spec <vaishnavisuresh588@gmail.com>
2025-11-12 12:10:45 +0530

    updated

--- array.py
+++ array.py
@@ -4,6 +4,10 @@ scores = [float(x) for x in sys.argv[1:]]
 
 total = sum(scores)
 average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
 
 print("Sum =", total)
 print("Average =", average)
+print("Maximum =", maximum)
+print("Minimum =", minimum)
